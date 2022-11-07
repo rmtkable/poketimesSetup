7796e3a6d787cffa934c87a50d63778e76330887
rmtkable <rmtkable@gmail.com>
2022-11-07 10:51:00 -0500

    setup for poketimes

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='container'>
+        <h4 className='center'>About</h4>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi qui labore culpa itaque est quas molestias magnam deleniti adipisci aliquam quo ullam, pariatur maxime unde molestiae quaerat voluptatibus veritatis repellat libero a optio incidunt animi dolor ducimus. Alias perspiciatis architecto tempore illum nemo ratione libero provident aut inventore tempora, id ducimus error iusto saepe exercitationem cum doloribus odio voluptatibus iure quasi recusandae culpa est in. Beatae veniam dolore facilis ipsa ut labore dolorem debitis cum sunt, minus laudantium, repellat reprehenderit iste ducimus nesciunt dolores, rem blanditiis! Iste aut minus pariatur placeat est. Quo, ducimus maxime! Architecto officia possimus porro blanditiis.</p>
+    </div>
+  )
+}
+
+export default About
